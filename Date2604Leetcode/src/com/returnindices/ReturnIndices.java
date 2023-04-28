52f53574ec870921093f489fa9a783b56aed1a72
sachinghatage <sachinghatage1997@gmail.com>
2023-04-28 14:59:57 +0530

    return indices

--- Date2604Leetcode/src/com/returnindices/ReturnIndices.java
+++ Date2604Leetcode/src/com/returnindices/ReturnIndices.java
@@ -19,6 +19,7 @@ public class ReturnIndices {
 	private static int[] twoSum(int[] nums, int target) {
 
 		Map<Integer,Integer> map=new HashMap<>();
+
 		for(int i=0;i<nums.length;i++) {
 			if(map.containsKey(target-nums[i])) {
 				 return new int[] {map.get(target-nums[i]),i};
