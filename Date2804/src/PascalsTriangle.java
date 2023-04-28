b9f8650e28e4e1530c5020273c0f6a9638347fb0
sachinghatage <sachinghatage1997@gmail.com>
2023-04-28 14:27:31 +0530

    pascals triangle

---
+++ Date2804/src/PascalsTriangle.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+
+	public static void main(String[] args) {
+
+		int numRows=5;
+		List<List<Integer>> triangle = generate(numRows);
+		System.out.println(triangle);
+	}
+
+	private static List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> triangle=new ArrayList<>();
+		if(numRows==0) return triangle;
+		
+		List<Integer> firstRow=new ArrayList<>();
+		firstRow.add(1);
+		triangle.add(firstRow);
+		
+		for(int i=1;i<numRows;i++) {
+			List<Integer> prevRow=triangle.get(i-1);
+			List<Integer> row=new ArrayList<>();
+			row.add(1);
+			
+			for(int j=1;j<i;j++) {
+				row.add(prevRow.get(j-1)+prevRow.get(j));    //here i and j are indexes
+			}
+			
+			row.add(1);
+			triangle.add(row);
+		}
+		
+		return triangle;
+		
+	}
+
+}
